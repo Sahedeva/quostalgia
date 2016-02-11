eb58f1479e608210ffc1984ac9449a1d8cc3e9d8
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2016-02-11 06:53:51 -0600

    improved styling and added more cast detail on click of actors name

--- myapp/static/movieJavascript.js
+++ myapp/static/movieJavascript.js
@@ -17,11 +17,29 @@ $(document).ready(function() {
             success: function(data, textStatus, jqXHR){
               console.log(data);
               console.log(data['credits']['cast'])
-              $("#results").append("<li class='movieListings'><h2>"+data['title']+"</h2><h5>"+data['release_date']+"</h5><a href='"+data['homepage']+"'>"+data['homepage']+"</a><br><img class='moviePoster' src='http://image.tmdb.org/t/p/w500/"+data['poster_path']+"'/><h4>"+data['tagline']+"</h4><h5>"+data['overview']+"</h5></li>");
+              var resultStr ="<li class='movieListings'><h2>"+data['title']+"</h2><h5>"+data['release_date']+"</h5><img class='backdrop' src='http://image.tmdb.org/t/p/w500/"+data['backdrop_path']+"'/><img class='moviePoster' src='http://image.tmdb.org/t/p/w500/"+data['poster_path']+"'/><br><a target='_blank' href='"+data['homepage']+"'>"+data['homepage']+"</a><br><h4>"+data['tagline']+"</h4><h5>"+data['overview']+"</h5></li><div class='castDiv'>";
               for (i=0;i<data['credits']['cast'].length;i++) {
-                $("#results").append("<li><img class='moviePoster' src='http://image.tmdb.org/t/p/w500/"+data['credits']['cast'][i]['profile_path']+"'/><br>Character: "+data['credits']['cast'][i]['character']+"<br>Played by: "+data['credits']['cast'][i]['name']+"</li>");
+                if(i%6==0){
+                  resultStr +="<div style='height: 220px max-height:220px overflow:auto' class='row'>"
+                }
+                resultStr += "<div class='col-xs-2'><br><img class='castPoster' src='http://image.tmdb.org/t/p/w500/"+data['credits']['cast'][i]['profile_path']+"'/><br><span style='color:red!important'>"+data['credits']['cast'][i]['character']+"</span><br><span class='castName' style='color:blue!important' title='"+data['credits']['cast'][i]['id']+"'>"+data['credits']['cast'][i]['name']+"</span></div>";
+                if((i+1)%6==0||i==(data['credits']['cast'].length-1)){
+                  resultStr +="</div>"
+                }
               }
-              
+              resultStr +="</div>"
+              $("#results").append(resultStr);
+              $(".castName").on('click', function(){
+                var personId = $(this).attr('title');
+                $.ajax({
+                  url: "http://api.themoviedb.org/3/person/"+personId+"?api_key=5da4693192fc98b8390ac6cebfc81c82",
+                  dataType: "json",
+                  method: "GET",
+                  success: function(data, textStatus, jqXHR){
+                    console.log(data)
+                  }
+                });
+              });
             },
           });
         }
