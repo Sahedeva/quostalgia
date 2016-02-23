9112cdb2f06a45395f48078b1c97aa8a435f6ba0
Robert Weilbaecher <robertweilbaecher@hotmail.com>
2016-02-22 22:50:24 -0600

    fixed styling on navbar

--- myapp/static/registerLogin.js
+++ myapp/static/registerLogin.js
@@ -1,14 +1,16 @@
 console.log("%c                                                 \n           __..--''``\\--....___   _..,_          \n       _.-'    .-/\";  `        ``<._  ``-+'~=.   \n   _.-' _..--.'_    \\                    `(^) )  \n  ((..-'    (< _     ;_..__               ; `'   \n             `-._,_)'      ``--...____..-'       \n                                                 \n                                                 \n            consoleCat - by Bob                  \n                                                 ", "color:yellow;background-color:black;");
 
-$('#loginSubmit').on('click', function(){
+$('#loginForm').on('submit', function(){
 	var name = $("#loginName").val();
 	var password = $("#loginPassword").val();
 	console.log(name,password);
+	return false;
 });
 
-$('#registerSubmit').on('click', function(){
+$('#registerForm').on('submit', function(){
 	var name = $("#registerName").val();
 	var password = $("#registerPassword").val();
 	var avatarUrl = $("#registerAvatar").val();
-	console.log(name,password);
+	console.log(name,password,avatarUrl);
+	return false;
 });
