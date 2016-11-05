6fee297ece2f61014383a644dbab929bce28ea28
Robert Weilbaecher <robertgweilbaecher@gmail.com>
2016-11-05 18:31:44 -0500

    trying to get app to launch from /

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.base, name='base'),
-    url(r'^movies/', views.movies, name='movies'),
-    url(r'^tvShows/', views.tvShows, name='tvShows'),
-]
+# from django.conf.urls import url
+#
+# from . import views
+#
+# urlpatterns = [
+#     url(r'^$', views.base, name='base'),
+#     url(r'^$/movies', views.movies, name='movies'),
+#     url(r'^$/tvShows', views.tvShows, name='tvShows'),
+# ]
